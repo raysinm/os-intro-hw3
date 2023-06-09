2a3119b5346fd7d60d37974a6fa553175c6123ac
raysinm <raysin.maya1@gmail.com>
2023-06-09 18:32:39 +0300

    Completed queue structure

--- webserver-files/queue.c
+++ webserver-files/queue.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "queue.h"
 
-struct QueueNode{
-    int fd;
-    QueueNode* next;
-};
 
-struct RequestQueue{
-    QueueNode* head;
-    QueueNode* tail;
-    int capacity;
-    int size;
-};
 
 QueueNode* QueueNode_create(int fd){
-    struct QueueNode* node = (QueueNode*)malloc(sizeof(node));
+    QueueNode* node = (QueueNode*)malloc(sizeof(node));
     node -> fd = fd;
     node -> next = NULL;
     return node;
@@ -23,7 +14,7 @@ QueueNode* QueueNode_create(int fd){
 
 
 RequestQueue* RequestQueue_create(int capacity){
-    struct RequestQueue* queue = (RequestQueue*)malloc(sizeof(queue));
+    RequestQueue* queue = (RequestQueue*)malloc(sizeof(queue));
     queue -> capacity = capacity;
     queue -> head = NULL;
     queue -> tail = NULL;
@@ -31,22 +22,62 @@ RequestQueue* RequestQueue_create(int capacity){
     return queue;
 }
 
-bool RequestQueue_isempty(struct RequestQueue* queue){
+bool RequestQueue_isempty(RequestQueue* queue){
     return queue->size == 0;
 }
 
-int RequestQueue_front(struct RequestQueue* queue){
-    if (RequestQueue_isempty(queue)){
-        return -1;
+
+QueueError RequestQueue_queue(RequestQueue* queue, int new_fd){
+    if (queue->size == queue->capacity){
+        return QUEUE_FULL;
     }
-    struct QueueNode* head = queue->head;
-    return head->fd;
+
+    QueueNode* tail = queue->tail;
+    QueueNode* new_node = QueueNode_create(new_fd);
+    if (tail == NULL){
+        queue->tail = new_node;
+        queue->head = new_node;
+        ++(queue->size);
+        return QUEUE_SUCCESS;
+    }
+
+    
+
+    tail->next = new_node;
+    queue->tail = tail->next;
+    ++(queue->size);
+    return QUEUE_SUCCESS;
 }
 
+int RequestQueue_dequeue(RequestQueue* queue, QueueError* error){
+    if (queue->size == 0){
+        *error = QUEUE_EMPTY;
+        return -1;
+    }
 
-//TODO: Change return type to enum to deal with errors in main
-int RequestQueue_queue(struct RequestQueue* queue, int new_fd){
-    struct QueueNode* tail = queue->tail;
-    QueueNode* new_node = QueueNode_create(new_fd);
-    //TODO: Finish
+    QueueNode* head = queue->head;
+    QueueNode* next_head = head->next;
+    
+    int ret_fd = head->fd;
+    free(head);
+    queue->head = next_head;
+    
+    --(queue->size);
+    if (queue->size == 0){
+        queue->tail = NULL;
+    }
+
+    *error = QUEUE_SUCCESS;
+
+    return ret_fd;
+}
+
+int RequestQueue_front(RequestQueue* queue, QueueError* error){
+    if (queue->size!=0 && queue->head){
+        *error = QUEUE_SUCCESS;
+        return queue->head->fd;
+    }
+
+    *error = QUEUE_EMPTY;
+    return -1;
 }
--- webserver-files/queue.h
+++ webserver-files/queue.h
@@ -1,9 +1,31 @@
 #include <stdbool.h>
+#include <stdio.h>
 
-//TODO: Enum
+typedef enum{
+    QUEUE_SUCCESS,
+    QUEUE_EMPTY,
+    QUEUE_FULL
+} QueueError;
 
-typedef struct QueueNode *QueueNode;
-typedef struct RequestQueue *RequestQueue;
+typedef struct QueueNode QueueNode;
+typedef struct RequestQueue RequestQueue;
 
-void initRequestQueue(struct RequeustQueue* queue, int capacity);
+struct QueueNode{
+    int fd;
+    QueueNode* next;
+};
+
+struct RequestQueue{
+    QueueNode* head;
+    QueueNode* tail;
+    int capacity;
+    int size;
+};
+
+QueueNode* QueueNode_create(int fd);
+
+RequestQueue* RequestQueue_create(int capacity);
 bool RequestQueue_isempty(RequestQueue* queue);
+int RequestQueue_dequeue(RequestQueue* queue, QueueError* error);
+QueueError RequestQueue_queue(RequestQueue* queue, int new_fd);
+int RequestQueue_front(RequestQueue* queue, QueueError* error);
